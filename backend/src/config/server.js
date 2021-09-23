546fb2aa8fa1ddbb4460a87c1ec19d7f00c0f4b4
Lucio <luciocarvalho10@hotmail.com.br>
2021-09-22 20:37:20 -0400

    configurando servidor express

---
+++ backend/src/config/server.js
@@ -0,0 +1,9 @@
+const port = 3003
+const backlog = () => console.log(`BACKEND is running on port ${port}`)
+const boryParser = require('body-parser')
+const server = require('express')()
+
+server.use(boryParser.urlencoded({extended: true}))
+server.use(boryParser.json())
+
+server.listen(port, backlog)
