a07bfe498f22d19fa0e5c92e0dbebca61ee4a9c9
Lucio <luciocarvalho10@hotmail.com.br>
2021-10-12 19:27:10 -0400

    criando estrutura principal do projeto

---
+++ frontend/src/main/app.jsx
@@ -0,0 +1,13 @@
+import 'modules/bootstrap/dist/css/bootstrap.min.css'
+import 'modules/font-awesome/css/font-awesome.min.css'
+
+import React from 'react'
+import About from '../about/about'
+import Todo from '../todo/todo'
+
+export default props => (
+  <div className="container">
+    <Todo />
+    <About />
+  </div>
+)
