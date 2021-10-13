98e40df9ae72788271228064ce06184778053217
Lucio <luciocarvalho10@hotmail.com.br>
2021-10-12 20:03:59 -0400

    criando rotas

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -2,12 +2,11 @@ import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
 
 import React from 'react'
-import About from '../about/about'
-import Todo from '../todo/todo'
+
+import Routes from '../template/routes'
 
 export default props => (
   <div className="container">
-    <Todo />
-    <About />
+    <Routes />
   </div>
 )
