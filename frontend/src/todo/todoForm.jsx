4634da39a5ec599ac88183d13fd401eb7bc47553
Lucio <luciocarvalho10@hotmail.com.br>
2021-10-14 19:20:41 -0400

    criando função para teclas de atalho

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -2,23 +2,38 @@ import React from 'react'
 import Grid from '../template/Grid'
 import IconButton from '../template/iconButton'
 
-export default props => (
-  <div>
-    <div className="todoForm" role="form">
-      <Grid cols="12 9 10">
-        <input
-          id="description"
-          className="form-control"
-          placeholder="Adicione uma tarefa"
-          onChange={props.handleChange}
-          value={props.description}
-        />
-      </Grid>
-      <Grid cols="12 3 2">
-        <IconButton style="primary" icon="plus" onClick={props.handleAdd}/>
-        <IconButton style="info" icon="search" onClick={props.handleSearch}/>
-        <IconButton style="default" icon="close" onClick={props.handleClear}/>
-      </Grid>
+export default props => {
+  const keyHandler = e => {
+    if (e.key === 'Enter') {
+      e.shiftKey ? props.handleSearch() : props.handleAdd()
+    } else if (e.key === 'Escape') {
+      props.handleClear()
+    }
+  }
+
+  return (
+    <div>
+      <div className="todoForm" role="form">
+        <Grid cols="12 9 10">
+          <input
+            id="description"
+            className="form-control"
+            placeholder="Adicione uma tarefa"
+            onKeyUp={keyHandler}
+            onChange={props.handleChange}
+            value={props.description}
+          />
+        </Grid>
+        <Grid cols="12 3 2">
+          <IconButton style="primary" icon="plus" onClick={props.handleAdd} />
+          <IconButton style="info" icon="search" onClick={props.handleSearch} />
+          <IconButton
+            style="default"
+            icon="close"
+            onClick={props.handleClear}
+          />
+        </Grid>
+      </div>
     </div>
-  </div>
-)
+  )
+}
