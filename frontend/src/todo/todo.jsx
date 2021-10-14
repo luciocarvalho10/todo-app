6e68bb9d5fd0d77b43f55e20a533cf057d5b4e27
Lucio <luciocarvalho10@hotmail.com.br>
2021-10-14 15:30:16 -0400

    criando evento adicionar e onChange

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -4,11 +4,31 @@ import TodoForm from './todoForm'
 import TodoList from './todoList'
 
 export default class Todo extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { description: '', list: [] }
+
+    this.handleApp = this.handleApp.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleApp() {
+    console.log(this.state.description)
+  }
+
+  handleChange(e) {
+    this.setState({ ...this.state, description: e.target.value })
+  }
+
   render() {
     return (
       <div>
         <PageHeader name="Tarefas" small="Cadastro" />
-        <TodoForm />
+        <TodoForm
+          description={this.state.description}
+          handleAdd={this.handleApp}
+          handleChange={this.handleChange}
+        />
         <TodoList />
       </div>
     )
--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -10,10 +10,12 @@ export default props => (
           id="description"
           className="form-control"
           placeholder="Adicione uma tarefa"
+          onChange={props.handleChange}
+          value={props.description}
         />
       </Grid>
       <Grid cols="12 3 2">
-        <IconButton style="primary" icon="plus"/>
+        <IconButton style="primary" icon="plus" onClick={props.handleAdd}/>
       </Grid>
     </div>
   </div>
