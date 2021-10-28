19c13f631b4ba720625a7755a10b60e6c0e500af
Lucio <luciocarvalho10@hotmail.com.br>
2021-10-28 19:47:08 -0400

    ativando redux devtools

--- frontend-redux/src/main/store.js
+++ frontend-redux/src/main/store.js
@@ -2,6 +2,9 @@ import { createStore } from "redux";
 import rootReducer from "./reducers";
 
 
-const store = createStore(rootReducer)
+const devTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+const store = createStore(rootReducer, devTools)
 
 export default store
